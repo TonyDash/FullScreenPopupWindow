79a0ba710e765da298231b4dc7541c507e3acc0b
TonyDash <13435648420@139.com>
2020-01-25 16:33:42 +0800

    屏幕工具类，用户获取屏幕高宽和是否显示虚拟导航栏、状态栏等。

--- app/src/main/java/com/cjy/fullscreenpopupwindow/OsUtil.kt
+++ app/src/main/java/com/cjy/fullscreenpopupwindow/OsUtil.kt
@@ -1,216 +1,151 @@
 package com.cjy.fullscreenpopupwindow
 
 import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Point
+import android.graphics.Rect
+import android.os.Build
+import android.util.DisplayMetrics
+import android.view.Display
+import android.view.View
+import android.view.Window
 
 class OsUtil {
 
     companion object{
-        public fun getScreenHeight(activity: Activity):Int{
-            activity.run {
-
-            }
+        private fun getScreenHeight(activity: Activity):Int{
             val display = activity.windowManager.defaultDisplay
-            var realHeight = 0
-            when{
-
+            var realHeight:Int
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                val metrics = DisplayMetrics()
+                display.getRealMetrics(metrics)
+                realHeight = metrics.heightPixels
+            }else{
+                val mGetRawH = Display::class.java.getMethod("getRawHeight")
+                realHeight = mGetRawH.invoke(display) as Int
             }
-            return 0
+            return realHeight
+        }
+
+        private fun getScreenWidth(activity: Activity):Int{
+            val display = activity.windowManager.defaultDisplay
+            var realWidth:Int
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                val metrics = DisplayMetrics()
+                display.getRealMetrics(metrics)
+                realWidth = metrics.widthPixels
+            }else{
+                val mGetRawH = Display::class.java.getMethod("getRawWidth")
+                realWidth = mGetRawH.invoke(display) as Int
+            }
+            return realWidth
+        }
+
+        /**
+         * 状态栏高度
+         */
+        fun getStatusBarHeight(activity: Activity):Int{
+            val resourceId = activity.resources.getIdentifier("status_bar_height", "dimen", "android")
+            return activity.resources.getDimensionPixelSize(resourceId)
+        }
+
+        /**
+         * 获取虚拟键高度（无论是否隐藏）
+         */
+        fun getNavigationBarHeight(context:Context):Int{
+            var result:Int
+            val resources = context.resources
+            val resourceId = resources.getIdentifier(if (isPortrait(context))"navigation_bar_height" else "navigation_bar_height_landscape", "dimen", "android")
+            if (resourceId>0)
+                result = context.resources.getDimensionPixelSize(resourceId)
+            else result = 0
+            return result
+        }
+
+        /**
+         * 手机具有底部导航栏时，底部导航栏是否可见
+         */
+        private fun isNavigationBarVisible(activity: Activity):Boolean{
+            var show = false
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+                val display = activity.window.windowManager.defaultDisplay
+                val point = Point()
+                display.getRealSize(point)
+                val decorView = activity.window.decorView
+                val conf = activity.resources.configuration
+                if (Configuration.ORIENTATION_LANDSCAPE == conf.orientation){
+                    val contentView:View = decorView.findViewById(R.id.content)
+                    show = (point.x!=contentView.width)
+                }else{
+                    val rect = Rect()
+                    decorView.getWindowVisibleDisplayFrame(rect)
+                    show = (rect.bottom!=point.y)
+                }
+            }
+            return show
+        }
+
+        /**
+         * 检测是否具有底部导航栏
+         */
+        private fun checkDeviceHasNavigationBar(activity: Activity):Boolean{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                val windowManager = activity.windowManager
+                val display = windowManager.defaultDisplay
+                val realDisplayMetrics = DisplayMetrics()
+                display.getRealMetrics(realDisplayMetrics)
+                val realHeight = realDisplayMetrics.heightPixels
+                val realWidth = realDisplayMetrics.widthPixels
+                val displayMetrics = DisplayMetrics()
+                display.getMetrics(displayMetrics)
+                val displayHeight = displayMetrics.heightPixels
+                val displayWidth = displayMetrics.widthPixels
+                return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0
+            }else{
+                var hasNavigationBar:Boolean
+                val resources = activity.resources
+                val id = resources.getIdentifier("config_showNavigationBar", "bool", "android")
+                if (id>0){
+                    hasNavigationBar = resources.getBoolean(id)
+                }
+                val systemPropertiesClass = Class.forName("android.os.SystemProperties")
+                val m = systemPropertiesClass.getMethod("get",String.javaClass)
+                val navBarOverride:String = m.invoke(systemPropertiesClass,"qemu.hw.mainkeys") as String
+                hasNavigationBar = !"1".equals(navBarOverride)
+                return hasNavigationBar
+            }
+        }
+
+        /**
+         * 获取当前底部栏高度（隐藏后高度为0）
+         */
+        private fun getCurrentNavigationBarHeight(activity: Activity):Int{
+            var navigationBarHeight = 0
+            val resources = activity.resources
+            val resourceId = resources.getIdentifier(if (isPortrait(activity))"navigation_bar_height" else "navigation_bar_height_landscape", "dimen", "android")
+            if (resourceId>0&& checkDeviceHasNavigationBar(activity)&& isNavigationBarVisible(activity)){
+                navigationBarHeight = resources.getDimensionPixelSize(resourceId)
+            }
+            return navigationBarHeight
+
+        }
+        private fun isPortrait(context: Context):Boolean{
+            return context.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+        }
+
+        /**
+         * 获取可用屏幕高度，排除虚拟键
+         */
+        fun getContentHeight(activity: Activity):Int{
+            return getScreenHeight(activity) - getCurrentNavigationBarHeight(activity)
+        }
+
+        /**
+         * 标题栏高度，如果隐藏了标题栏则返回0
+         */
+        fun getTitleHeight(activity: Activity):Int{
+            return activity.window.findViewById<View>(Window.ID_ANDROID_CONTENT).top
         }
     }
-
-//    public static int getScreenHeight(Activity activity) {
-//        if (null == activity) {
-//            return 0;
-//        }
-//        Display display = activity.getWindowManager().getDefaultDisplay();
-//        int realHeight = 0;
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-//            final DisplayMetrics metrics = new DisplayMetrics();
-//            display.getRealMetrics(metrics);
-//            realHeight = metrics.heightPixels;
-//        } else {
-//            try {
-//                Method mGetRawH = Display.class.getMethod("getRawHeight");
-//                realHeight = (int) mGetRawH.invoke(display);
-//            } catch (NoSuchMethodException e) {
-//                e.printStackTrace();
-//            } catch (IllegalAccessException e) {
-//                e.printStackTrace();
-//            } catch (InvocationTargetException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return realHeight;
-//    }
-//
-//    public static int getScreenWidth(Activity activity) {
-//        if (null == activity) {
-//            return 0;
-//        }
-//        Display display = activity.getWindowManager().getDefaultDisplay();
-//        int realWidth = 0;
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-//            final DisplayMetrics metrics = new DisplayMetrics();
-//            display.getRealMetrics(metrics);
-//            realWidth = metrics.widthPixels;
-//        } else {
-//            try {
-//                Method mGetRawH = Display.class.getMethod("getRawWidth");
-//                realWidth = (int) mGetRawH.invoke(display);
-//            } catch (NoSuchMethodException e) {
-//                e.printStackTrace();
-//            } catch (IllegalAccessException e) {
-//                e.printStackTrace();
-//            } catch (InvocationTargetException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return realWidth;
-//    }
-//
-//    /**
-//     * 状态栏高度
-//     *
-//     * @param activity
-//     * @return
-//     */
-//    public static int getStatusBarHeight(Activity activity) {
-//        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
-//        return activity.getResources().getDimensionPixelSize(resourceId);
-//    }
-//
-//    /**
-//     * 获取虚拟键高度（无论是否隐藏）
-//     */
-//    public static int getNavigationBarHeight(Context context) {
-//        int result = 0;
-//        Resources resources = context.getResources();
-//        int resourceId = resources.getIdentifier(isPortrait(context) ? "navigation_bar_height" : "navigation_bar_height_landscape", "dimen", "android");
-//        if (resourceId > 0) {
-//            result = context.getResources().getDimensionPixelSize(resourceId);
-//        }
-//        return result;
-//    }
-//
-//    /**
-//     * 手机具有底部导航栏时，底部导航栏是否可见
-//     *
-//     * @param activity
-//     * @return
-//     */
-//    private static boolean isNavigationBarVisible(Activity activity) {
-//        boolean show = false;
-//        try {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-//                Display display = activity.getWindow().getWindowManager().getDefaultDisplay();
-//                Point point = new Point();
-//                display.getRealSize(point);
-//                View decorView = activity.getWindow().getDecorView();
-//                Configuration conf = activity.getResources().getConfiguration();
-//                if (Configuration.ORIENTATION_LANDSCAPE == conf.orientation) {
-//                    View contentView = decorView.findViewById(R.id.content);
-//                    show = (point.x != contentView.getWidth());
-//                } else {
-//                    Rect rect = new Rect();
-//                    decorView.getWindowVisibleDisplayFrame(rect);
-//                    show = (rect.bottom != point.y);
-//                }
-//            }
-//        } catch (Exception e) {
-//            KLog.e(e);
-//        }
-//        return show;
-//    }
-//
-//    /**
-//     * 检测是否具有底部导航栏
-//     */
-//    private static boolean checkDeviceHasNavigationBar(Activity activity) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-//            WindowManager windowManager = activity.getWindowManager();
-//            Display display = windowManager.getDefaultDisplay();
-//            DisplayMetrics realDisplayMetrics = new DisplayMetrics();
-//            display.getRealMetrics(realDisplayMetrics);
-//            int realHeight = realDisplayMetrics.heightPixels;
-//            int realWidth = realDisplayMetrics.widthPixels;
-//            DisplayMetrics displayMetrics = new DisplayMetrics();
-//            display.getMetrics(displayMetrics);
-//            int displayHeight = displayMetrics.heightPixels;
-//            int displayWidth = displayMetrics.widthPixels;
-//            return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
-//        } else {
-//            boolean hasNavigationBar = false;
-//            Resources resources = activity.getResources();
-//            int id = resources.getIdentifier("config_showNavigationBar", "bool", "android");
-//            if (id > 0) {
-//                hasNavigationBar = resources.getBoolean(id);
-//            }
-//            try {
-//                Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
-//                Method m = systemPropertiesClass.getMethod("get", String.class);
-//                String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
-//                if ("1".equals(navBarOverride)) {
-//                    hasNavigationBar = false;
-//                } else {
-//                    hasNavigationBar = true;
-//                }
-//            } catch (ClassNotFoundException e) {
-//                e.printStackTrace();
-//            } catch (NoSuchMethodException e) {
-//                e.printStackTrace();
-//            } catch (IllegalAccessException e) {
-//                e.printStackTrace();
-//            } catch (InvocationTargetException e) {
-//                e.printStackTrace();
-//            }
-//            return hasNavigationBar;
-//        }
-//    }
-//
-//    /**
-//     * 获取当前底部栏高度（隐藏后高度为0）
-//     *
-//     * @param activity
-//     * @return
-//     */
-//    public static int getCurrentNavigationBarHeight(Activity activity) {
-//        int navigationBarHeight = 0;
-//        Resources resources = activity.getResources();
-//        int resourceId = resources.getIdentifier(isPortrait(activity) ? "navigation_bar_height" : "navigation_bar_height_landscape", "dimen", "android");
-//        if (resourceId > 0 && checkDeviceHasNavigationBar(activity) && isNavigationBarVisible(activity)) {
-//            navigationBarHeight = resources.getDimensionPixelSize(resourceId);
-//        }
-//        return navigationBarHeight;
-//    }
-//
-//    /**
-//     * 是否为竖屏
-//     *
-//     * @param context
-//     * @return
-//     */
-//    private static boolean isPortrait(Context context) {
-//        return context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-//    }
-//
-//    /**
-//     * 获取可用屏幕高度，排除虚拟键
-//     *
-//     * @param activity
-//     * @return
-//     */
-//    public static int getContentHeight(Activity activity) {
-//        int contentHeight = getScreenHeight(activity) - getCurrentNavigationBarHeight(activity);
-//        return contentHeight;
-//    }
-//
-//    /**
-//     * 标题栏高度，如果隐藏了标题栏则返回0
-//     *
-//     * @param activity
-//     * @return
-//     */
-//    public static int getTitleHeight(Activity activity) {
-//        return activity.getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
-//    }
 }
